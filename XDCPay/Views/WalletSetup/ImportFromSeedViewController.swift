475889d0a73a6a4e06d74f377c04de3b8eb7f57a
ChhayaSinghParihar <chhaya13111981@gmail.com>
2022-03-24 18:28:46 +0530

    Onboarding Slide UI complete, setup wallet UI flow started

---
+++ XDCPay/Views/WalletSetup/ImportFromSeedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ImportFromSeedViewController.swift
+//  XDCPay
+//
+//  Created by Chhaya Singh Parihar on 24/03/22.
+//
+
+import UIKit
+
+class ImportFromSeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
---
+++ XDCPay/Views/WalletSetup/LandingPageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LandingPageViewController.swift
+//  XDCPay
+//
+//  Created by Chhaya Singh Parihar on 24/03/22.
+//
+
+import UIKit
+
+class LandingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
