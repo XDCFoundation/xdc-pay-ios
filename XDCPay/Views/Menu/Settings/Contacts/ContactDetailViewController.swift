1c5b883f231d3f92c0715c354f009fd32baafa9f
Piyush Kumar <piyushkumar@eezylife.in>
2022-05-20 11:23:02 +0530

    show contact details

--- XDCPay/Views/Menu/Settings/Contacts/ContactDetailViewController.swift
+++ XDCPay/Views/Menu/Settings/Contacts/ContactDetailViewController.swift
@@ -8,18 +8,31 @@
 import UIKit
 
 class ContactDetailViewController: UIViewController {
-
-    var contactName = ""
     
+    @IBOutlet weak var noTransaction: UILabel!
+    var contactName = ""
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var address: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(contactName)
+        
+        let contacts = DataBaseManager.shared.getContacts()
+        let contact =  contacts.filter{$0.name == contactName}.first
+        self.name.text = contact!.name
+        self.address.text = contact!.address
+    
     }
     
 
     @IBAction func onEdit(_ sender: Any) {
         
         self.pVC(viewConterlerId: "EditContactViewController") 
+    }
+    
+    @IBAction func onCopy(_ sender: Any) {
+          showCopyToast()
+          UIPasteboard.general.string = self.address.text!
     }
     
 
