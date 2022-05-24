8a19cf0ad3602274846681c9a331e1a90d105c6a
Piyush Kumar <piyushkumar@eezylife.in>
2022-05-24 10:51:29 +0530

    bug fixes

--- XDCPay/Extentions/Double.swift
+++ XDCPay/Extentions/Double.swift
@@ -15,3 +15,9 @@ extension Double {
         return (self * divisor).rounded() / divisor
     }
 }
+extension Double {
+    func roundToDecimal(_ fractionDigits: Int) -> Double {
+        let multiplier = pow(10, Double(fractionDigits))
+        return Darwin.round(self * multiplier) / multiplier
+    }
+}
