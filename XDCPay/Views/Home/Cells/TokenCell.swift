11e2a56c5d87dca2d822881bce8080fe78dd5d3f
Angshuman-AngshumanLeewayhertz <angshuman@leewayhertz.com>
2022-05-26 10:45:51 +0530

    XI-76

--- XDCPay/Views/Home/Cells/TokenCell.swift
+++ XDCPay/Views/Home/Cells/TokenCell.swift
@@ -7,10 +7,17 @@
 
 import UIKit
 
+
 class TokenCell: UITableViewCell {
 
     @IBOutlet weak var tokenImg: UIImageView!
     @IBOutlet weak var tokenDetails: UILabel!
     
+    @IBAction func tokenOptions(_ sender: UIButton) {
+       // homeVc.pVC(viewConterlerId: "TokenOptionVC")
+        
+        let pickerViewClass = homeVc.storyboard?.instantiateViewController(withIdentifier: "TokenOptionVC") as! TokenOptionVC
+        homeVc.present(pickerViewClass, animated: true, completion: nil)
+    }
     
 }
